964b69dc00dae277fc0f1dbf6c5bd18ff6d8f7da
Java-Dsa <abhineshsahu2004@gmail.com>
2026-04-12 00:28:09 +0530

    Added  Basic problem programs

--- src/Basics/No_of_divisors.java
+++ src/Basics/No_of_divisors.java
@@ -1,14 +1,14 @@
 package Basics;
 
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class No_of_divisors {
     static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter number");
         int n=sc.nextInt();
-        ArrayList L=new ArrayList();
+        ArrayList<Integer> L = new ArrayList<>();
         for(int i=1 ; i<=n ; i++){
             if(n%i==0){
                 L.add(i);
