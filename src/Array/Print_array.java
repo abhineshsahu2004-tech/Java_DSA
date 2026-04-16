f8a8c1ce75247060862f00eeb8c7179d111ab218
Java-Dsa <abhineshsahu2004@gmail.com>
2026-04-16 23:55:02 +0530

    Added  Array problem

---
+++ src/Array/Print_array.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class Print_array {
+    static void print(int[]arr){
+        int n=arr.length;
+        for(int i=0 ;i<n;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
---
+++ src/Array/Squares_of_sorted_array.java
@@ -0,0 +1,26 @@
+package Array;
+
+import static Array.Print_array.print;
+
+public class Squares_of_sorted_array {
+    static int[] Square_Sorted_Array(int [] arr){
+        int n=arr.length;
+        int l=0,r=n-1;
+        int []result =new int[n];
+        for(int i=n-1;i>=0;i--){
+            if(arr[l]*arr[l]<=arr[r]*arr[r]){
+                result[i]=arr[r]*arr[r];
+                r--;
+            }
+            else {
+                result[i]=arr[l]*arr[l];
+                l++;
+            }
+        }
+        return result;
+    }
+    static void main(String[] args) {
+        int[] Array ={-4,-2,0,5,8};
+        print(Square_Sorted_Array(Array));
+    }
+}
