9966854df220504b4b1479996ec0d7092b03375a
Java-Dsa <abhineshsahu2004@gmail.com>
2026-04-19 22:56:02 +0530

    Added  Array problem

--- src/Array/Print_array.java
+++ src/Array/Print_array.java
@@ -1,9 +1,9 @@
 package Array;
 
 public class Print_array {
-    static void print(int[]arr){
-        int n=arr.length;
-        for(int i=0 ;i<n;i++){
+    static void print(int[]arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
     }
---
+++ src/Array/Remove_duplicates.java
@@ -0,0 +1,28 @@
+package Array;
+
+import java.util.*;
+
+public class Remove_duplicates {
+ static int remove_duplicate(int[]arr){
+    int n=arr.length;
+    int i=0;
+    int count=1;
+   for (int j=1;j<n;j++){
+       if (arr[i]!=arr[j]){
+           arr[i+1]=arr[j];
+           i++;
+           count++;
+       }
+   }
+//printing the unique elements
+   for (int k=0;k<count;k++){
+       System.out.print(arr[k]+" ");
+   }
+     System.out.println();
+     return count ;
+}
+public static void main(String[] args) {
+        int[] Happy = {0, 1, 2,2 , 6};
+    System.out.println(remove_duplicate(Happy));
+    }
+}
