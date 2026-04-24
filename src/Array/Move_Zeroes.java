50954eb4016fa87720cd390c431681f6a5637721
Java-Dsa <abhineshsahu2004@gmail.com>
2026-04-24 23:30:30 +0530

    Added  Array problem

---
+++ src/Array/Move_Zeroes.java
@@ -0,0 +1,20 @@
+package Array;
+
+import static Array.Print_array.print;
+
+public class Move_Zeroes {
+    static void main(String[] args) {
+        int[] arr = {0, 1, 2,2 , 6};
+        int n=arr.length;
+        int i=0,temp;
+        for(int j=0;j<n;j++){
+            if(arr[j]!=0){
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                i++;
+            }
+        }
+        print(arr);
+    }
+}
