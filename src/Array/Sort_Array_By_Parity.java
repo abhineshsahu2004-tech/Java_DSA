7b516f89e6a9a842afe567ec1e73509372c26149
Java-Dsa <abhineshsahu2004@gmail.com>
2026-04-24 23:19:14 +0530

    Added  Array problem

---
+++ src/Array/Sort_Array_By_Parity.java
@@ -0,0 +1,21 @@
+package Array;
+
+import static Array.Print_array.print;
+
+public class Sort_Array_By_Parity {
+    static void main(String[] args) {
+        int [] arr={1,2,3,3,4,0,6,2};
+        int n=arr.length;
+        int i=0;
+        int temp=0;
+        for (int j=0;j<n;j++){
+            if (arr[j]%2==0){
+                temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+                i++;
+            }
+        }
+        print(arr);
+    }
+}
