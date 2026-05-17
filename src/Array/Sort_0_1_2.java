3e13c6f0ca786cfa9f1949ad9225cbd7e01d0f89
Java-Dsa <abhineshsahu2004@gmail.com>
2026-05-18 00:32:45 +0530

    Added  Array problem

--- src/Array/Sort_0_1_2.java
+++ src/Array/Sort_0_1_2.java
@@ -1,27 +1,34 @@
 package Array;
+import java.util.Arrays;
 
 public class Sort_0_1_2 {
-    static void main(String[] args) {
-
-    }
-    static int[] sort012(int [] arr){
-        int n=arr.length;
-        int temp=0;
-        int low=0;
-        int mid=0;
-        int high=n-1;
-        for (int i=0;i<n;i++){
-            if (mid[i]==0){
-                temp=arr[low];
-                arr[low]=arr[mid];
-                arr[mid]=temp;
-                mid++;
+    static int[] sort012(int [] arr) {
+        int n = arr.length;
+        int temp = 0;
+        int low = 0;
+        int mid = 0;
+        int high = n - 1;
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
                 low++;
-            }
-            if (mid[i]==1){
                 mid++;
             }
-            if (mid[i]==2){}
+            else if (arr[mid] == 2) {
+                temp = arr[high];
+                arr[high] = arr[mid];
+                arr[mid] = temp;
+                high--;
+            } else{
+                mid++;
+            }
         }
+        return arr;
+    }
+    static void main(String[] args) {
+        int [] arr={1,0,2,0,2,2};
+        System.out.println(Arrays.toString(sort012(arr)));
     }
 }
