bdf332c984d2b70307d38596c37f39cb467d6521
Java-Dsa <abhineshsahu2004@gmail.com>
2026-09-01 01:15:38 +0530

    Exploring Binary Search

---
+++ src/BinarySearch/UpperBound.java
@@ -0,0 +1,28 @@
+package BinarySearch;
+
+public class UpperBound {
+    static int UB(int[] arr,int target){
+        int n=arr.length;
+        int i=0;
+        int j=n-1;
+        int ans=n;
+        while(i<j){
+            int mid=(i+j)/2;
+            if(arr[mid]==target){
+                return mid+1;
+            }
+            if(arr[mid]<target){
+                i=mid+1;
+            }
+            else {
+                ans=mid;
+                j=mid-1;
+            }
+        }
+        return ans;
+    }
+    static void main(String[] args) {
+        int[] arr={1,2,3,5,6,6,7,8,8};
+        System.out.println(UB(arr,7));
+    }
+}
